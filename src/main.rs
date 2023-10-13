4380f706fb6a6052c17c58d3dd00ad453c11a05f
Colin Wee <2gbleh@gmail.com>
2023-10-13 14:50:41 -0700

    write test to run per example

--- src/main.rs
+++ src/main.rs
@@ -8,3 +8,18 @@ fn main()
         println!("not true");
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let hay = "\
+path/to/foo:54:Blue Harvest
+path/to/bar:90:Something, Something, Something, Dark Side
+path/to/baz:3:It's a Trap!
+";
+        let haystack = "yeah";
+        assert!(mylib::checkit(&hay).unwrap());
+        assert!(mylib::checkit(&haystack).unwrap() == false);
+    }
+}
